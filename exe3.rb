70df59ee8f4747f508f1297e0df0b0fecaa89504
saipratap99 <122005067@sastra.ac.in>
2020-01-01 23:43:53 +0530

    numeric_functions_ruby

---
+++ exe3.rb
@@ -0,0 +1,23 @@
+a=0
+b=10
+# i or i()
+puts 5+b.i
+# zero? or zero?()
+c=-9
+d=c.i
+puts c.zero?
+puts a.zero?
+puts d.zero?
+# div()
+e=15
+puts e.div(-b)
+#quo
+puts e.quo(b)
+#f=b
+f=b.dup
+puts b
+puts c.arg
+# real and img
+num=Complex(-5,3)
+puts num.real,num.imag
+
